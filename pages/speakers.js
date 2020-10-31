a8d30b4571d7f749696768d4b3f0f2da5004c31e
Qing Ma <gsmofgsm@hotmail.com>
2020-10-31 10:08:15 +0100

    Speakers page

---
+++ pages/speakers.js
@@ -0,0 +1,13 @@
+function Page() {
+    return (
+        <div>
+            <img src="images/header.png" />
+            <img src="images/menu.gif" />
+            <img src="images/searchbar.gif" />
+            <img src="images/speakers.png" />
+            <img src="images/footer.png" />
+        </div>
+    );
+}
+
+export default Page;

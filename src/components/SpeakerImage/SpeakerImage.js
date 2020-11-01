ca8fbc6b1d64ce074d6c4226c2d2d8502deeccd0
Qing Ma <gsmofgsm@hotmail.com>
2020-11-01 23:53:35 +0100

    lazy load image with SimpleImg from react-simple-img

--- src/components/SpeakerImage/SpeakerImage.js
+++ src/components/SpeakerImage/SpeakerImage.js
@@ -1,7 +1,14 @@
-const SpeakerImage = ({id}) => (
-    <img
-        src={`/speakers/speaker-${id}.jpg`}
+import { SimpleImg } from 'react-simple-img';
+
+const SpeakerImage = ({id}) => {
+    const imageUrl = `/speakers/speaker-${id}.jpg`;
+    return <SimpleImg
+        src={imageUrl}
+        animationDuration="1"
+        width={200}
+        height={200}
+        applyAspectRatio="true"
     />
-);
+};
 
 export default SpeakerImage;

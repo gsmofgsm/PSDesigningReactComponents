d3cf93838e0f77f2057322c150ed72c72eeb89b9
Qing Ma <gsmofgsm@hotmail.com>
2020-11-01 13:21:30 +0100

    render speakers based on array of data

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
-const Speakers = () => <img src="images/speakers.png" />;
+const Speakers = () => {
+    const speakers = [
+        {
+            imageSrc: 'speaker-component-1124', name: 'Douglas Crockford'
+        },
+        {
+            imageSrc: 'speaker-component-1530', name: 'Tamara Baker'
+        },
+        {
+            imageSrc: 'speaker-component-10803', name: 'Eugene Chuvyrov'
+        },
+    ]
+    return (
+        <div>
+            {speakers.map(({imageSrc, name}) => {
+                return <img src={`/images/${imageSrc}.png`}
+                    alt={name} key={imageSrc} />
+            })}
+        </div>
+    )
+};
 
 export default Speakers;

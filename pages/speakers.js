aa14b488596dc0b8db355015c508526cbb0f70fe
Qing Ma <gsmofgsm@hotmail.com>
2020-11-01 16:53:00 +0100

    Layout Component

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import Footer from '../src/components/Footer/Footer';
-import Header from '../src/components/Header/Header';
-import Menu from '../src/components/Menu/Menu';
+import Layout from '../src/components/Layout/Layout';
 import Speakers from '../src/components/Speakers/Speakers';
-import SpeakerSearchBar from '../src/components/SpeakerSearchBar/SpeakerSearchBar';
 
 function Page() {
-    const speakers = [
-        {
-            imageSrc: 'speaker-component-1124', name: 'Douglas Crockford'
-        },
-        {
-            imageSrc: 'speaker-component-1530', name: 'Tamara Baker'
-        },
-        {
-            imageSrc: 'speaker-component-10803', name: 'Eugene Chuvyrov'
-        },
-    ];
-
     return (
-        <div>
-            <Header />
-            <Menu />
-            <SpeakerSearchBar />
+        <Layout>
             <Speakers />
-            <Footer />
-        </div>
+        </Layout>
     );
 }
 

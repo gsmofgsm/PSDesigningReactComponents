b723126f8d17d4112b0034cdfad8765c1c4ad765
Qing Ma <gsmofgsm@hotmail.com>
2020-11-02 23:39:21 +0100

    use placeholder image for errorUI

--- src/components/Speaker/Speaker.js
+++ src/components/Speaker/Speaker.js
@@ -3,33 +3,52 @@ import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 import SpeakerFavoriteButton from '../SpeakerFavoriteButton/SpeakerFavoriteButton';
 import SpeakerImage from '../SpeakerImage/SpeakerImage';
 
-const SpeakerComponent = ({ id, firstName, lastName, bio, isFavorite, put }) => (
-    <div className="rounded overflow-hidden shadow-lg p-6 bg-white" key={id}>
+const SpeakerComponent = ({ id, firstName, lastName, bio, isFavorite, put, showErrorCard }) => {
+
+  if (showErrorCard) {
+    return (
+      <div className="rounded overflow-hidden shadow-lg p-6 bg-white">
         <div className="grid grid-cols-4 mb-6">
-        <div className="font-bold text-lg col-span-3">{`${firstName} ${lastName}`}</div>
-        <div className="flex justify-end">
-            <SpeakerFavoriteButton isFavorite={isFavorite} 
-                onFavoriteToggle={() => {
-                    put({
-                        id,
-                        firstName,
-                        lastName,
-                        bio,
-                        isFavorite: !isFavorite,
-                    });
-                }}/>
-        </div>
+          <div className="font-bold text-lg col-span-3">
+            Error Showing Speaker
+          </div>
         </div>
         <div className="mb-6">
-            <SpeakerImage id={id} />
+          <img src="/speakers/dummy-speaker-image.jpg" />
         </div>
-        <div className="text-gray-600">{bio.substr(0, 70) + '...'}</div>
-    </div>
-);
+        <div>Contact site owner for resolution.</div>
+      </div>
+    );
+  }
+
+    return (
+        <div className="rounded overflow-hidden shadow-lg p-6 bg-white" key={id}>
+            <div className="grid grid-cols-4 mb-6">
+            <div className="font-bold text-lg col-span-3">{`${firstName} ${lastName}`}</div>
+            <div className="flex justify-end">
+                <SpeakerFavoriteButton isFavorite={isFavorite} 
+                    onFavoriteToggle={() => {
+                        put({
+                            id,
+                            firstName,
+                            lastName,
+                            bio,
+                            isFavorite: !isFavorite,
+                        });
+                    }}/>
+            </div>
+            </div>
+            <div className="mb-6">
+                <SpeakerImage id={id} />
+            </div>
+            <div className="text-gray-600">{bio.substr(0, 70) + '...'}</div>
+        </div>
+    );
+};
 
 const Speaker = React.memo((props) => {
     return (
-        <ErrorBoundary errorUI={<div>Something went Wrong in the Speaker Card</div>}>
+        <ErrorBoundary errorUI={<SpeakerComponent showErrorCard={true}></SpeakerComponent>}>
             <SpeakerComponent {...props} />
         </ErrorBoundary>
     );

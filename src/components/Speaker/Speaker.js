f96a7b42ad61ad9f8f287e6cb40f2969e506eebd
Qing Ma <gsmofgsm@hotmail.com>
2020-11-02 23:28:47 +0100

    Add ErrorBoundaries for Speaker

--- src/components/Speaker/Speaker.js
+++ src/components/Speaker/Speaker.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 import SpeakerFavoriteButton from '../SpeakerFavoriteButton/SpeakerFavoriteButton';
 import SpeakerImage from '../SpeakerImage/SpeakerImage';
 
-const Speaker = React.memo(({ id, firstName, lastName, bio, isFavorite, put }) => (
+const SpeakerComponent = ({ id, firstName, lastName, bio, isFavorite, put }) => (
     <div className="rounded overflow-hidden shadow-lg p-6 bg-white" key={id}>
         <div className="grid grid-cols-4 mb-6">
         <div className="font-bold text-lg col-span-3">{`${firstName} ${lastName}`}</div>
@@ -24,6 +25,14 @@ const Speaker = React.memo(({ id, firstName, lastName, bio, isFavorite, put }) =
         </div>
         <div className="text-gray-600">{bio.substr(0, 70) + '...'}</div>
     </div>
-));
+);
+
+const Speaker = React.memo((props) => {
+    return (
+        <ErrorBoundary>
+            <SpeakerComponent {...props} />
+        </ErrorBoundary>
+    );
+});
 
 export default Speaker;

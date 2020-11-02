50855d03273a0b34d3cfc10c10dd3fca50eb10aa
Qing Ma <gsmofgsm@hotmail.com>
2020-11-02 15:21:32 +0100

    use Render Props instead of HOCs

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -4,76 +4,84 @@ import SpeakerSearchBar from '../SpeakerSearchBar/SpeakerSearchBar';
 
 import { REQUEST_STATUS } from '../../reducers/request';
 
-import withRequest from '../HOCs/withRequest';
-import withSpecialMessage from '../HOCs/withSpecialMessage';
-
-import { compose } from 'recompose';
-
-const Speakers = ({ records: speakers, status, error, put, bgColor, specialMessage }) => {
-    function toggleSpeakerFavorite(speakerRec) {
-      return {
-        ...speakerRec,
-        isFavorite: !speakerRec.isFavorite,
-      };
-    }
-
-    const onFavoriteToggleHandler = async (speakerRec) => {
-      put({
-        ...speakerRec,
-        isFavorite: !speakerRec.isFavorite
-      });
-    }
+const Speakers = ({ bgColor }) => {
 
     const [searchQuery, setSearchQuery] = useState("");
-
-    const success = status === REQUEST_STATUS.SUCCESS;
-    const isLoading = status === REQUEST_STATUS.LOADING;
-    const hasErrored = status === REQUEST_STATUS.ERROR;
 
     return (
         <div className={bgColor}>
             <SpeakerSearchBar searchQuery={searchQuery}
               setSearchQuery={setSearchQuery} />
 
-            {specialMessage && specialMessage.length > 0 && (
-              <div
-                className="bg-orange-100 border-l-8 border-orange-500 text-orange-700 p-4 text-2xl"
-                role="alert"
-              >
-                <p className="font-bold">Special Message</p>
-                <p>{specialMessage}</p>
-              </div>
-            )}
+            <>
+              <SpecialMessageRenderProps>
+                {({ specialMessage }) => {
+                  return (
+                    <Request baseUrl="http://localhost:4000" routeName="speakers">
+                      {({ records: speakers, status, error, put }) => {
+                        const onFavoriteToggleHandler = async (speakerRec) => {
+                          put({
+                            ...speakerRec,
+                            isFavorite: !speakerRec.isFavorite,
+                          });
+                        };
+                        const success = status === REQUEST_STATUS.SUCCESS;
+                        const isLoading = status === REQUEST_STATUS.LOADING;
+                        const hasErrored = status === REQUEST_STATUS.ERROR;
 
-            {isLoading && <div>Loading...</div>}
-            {hasErrored && (
-              <div>
-                Loading error... Is the json-server running? try "npm run json-server" 
-                at terminal prompt
-                <br />
-                <b>ERROR: {error.message}</b>
-              </div>
-            )}
-            {success && (
-              <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
-                  {speakers
-                    .filter((rec) => {
-                      const targetString = `${rec.firstName} ${rec.lastName}`.toLowerCase();
-                      return searchQuery.length === 0 ? true :targetString.includes(searchQuery.toLowerCase());
-                    })
-                    .map((speaker) => (
-                      <Speaker key={speaker.id} {...speaker}
-                              onFavoriteToggle={() => onFavoriteToggleHandler(speaker)} />
-                    ))}
-              </div>)}
+                        return (
+                          <>
+                            {specialMessage && specialMessage.length > 0 && (
+                              <div
+                                className="bg-orange-100 border-l-8 border-orange-500 text-orange-700 p-4 text-2xl"
+                                role="alert"
+                              >
+                                <p className="font-bold">Special Message</p>
+                                <p>{specialMessage}</p>
+                              </div>
+                            )}
+
+                            {isLoading && <div>Loading...</div>}
+                            {hasErrored && (
+                              <div>
+                                Loading error... Is the json-server running? (try "npm
+                                run json-server" at terminal prompt)
+                                <br />
+                                <b>ERROR: {error.message}</b>
+                              </div>
+                            )}
+                            {success && (
+                              <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
+                                {speakers
+                                  .filter((rec) => {
+                                    const targetString = `${rec.firstName} ${rec.lastName}`.toLowerCase();
+                                    return searchQuery.length === 0
+                                      ? true
+                                      : targetString.includes(
+                                          searchQuery.toLowerCase(),
+                                        );
+                                  })
+                                  .map((speaker) => (
+                                    <Speaker
+                                      key={speaker.id}
+                                      {...speaker}
+                                      onFavoriteToggle={() =>
+                                        onFavoriteToggleHandler(speaker)
+                                      }
+                                    />
+                                  ))}
+                              </div>
+                            )}
+                          </>
+                        );
+                      }}
+                    </Request>
+                  );
+                }}
+              </SpecialMessageRenderProps>
+            </>
         </div>
     )
 };
 
-// export default withSpecialMessage()(
-//   withRequest('http://localhost:4000', 'speakers')(Speakers));
-
-export default compose(
-  withRequest('http://localhost:4000', 'speakers'),
-  withSpecialMessage()
-)(Speakers);
+export default Speakers;
